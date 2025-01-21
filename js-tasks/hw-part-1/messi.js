4ae9ddfe2c5765354a709e30f925bb7a2dafca39
Dmitry Hrunt <fantomys17@gmail.com>
2025-01-21 15:53:13 +0200

    made the codewars messi assigment

--- js-tasks/hw-part-1/messi.js
+++ js-tasks/hw-part-1/messi.js
@@ -3,3 +3,9 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
     return +laLigaGoals + +copaDelReyGoals + +championsLeagueGoals;
 }
 // End Anastasiia Kravchenko
+
+//Hrunt Gmitry
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+//End Hrunt Gmitry
