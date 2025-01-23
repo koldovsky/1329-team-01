213a389698b92e64a3545658f7a21ce31e3f06be
Nikita Demianov <hourlystory@gmail.com>
2025-01-23 18:53:45 +0200

    add a structure for js-tasks-2

---
+++ js-tasks/tasks-02.js
@@ -0,0 +1,27 @@
+//Базові задачі:
+//https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+
+//https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+
+//Поглиблені задачі:
+//https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+
+//https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+
+
+
+//-------------------------------------------------------------------------------------------------//
+//Додаткові задачі:
+// Double Integer    https://www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+
+// Twice as old    https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+
+// Return n-th even number    https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+// What's the real floor     https://www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+
+// Clock    https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+// Is n divisible by x and y    https://www.codewars.com/kata/5545f109004975ea66000086/train/javascript
+
+
