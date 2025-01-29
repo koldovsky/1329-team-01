809de63184f6f38a6703d1ba591dde82cea5e275
Anastasiia <yourgracefulproblem@gmail.com>
2025-01-29 16:45:58 +0200

    JS added for hire-us-form

---
+++ js/index.hire-us-form.js
@@ -0,0 +1,29 @@
+const inputElements = document.querySelectorAll('input');
+const submitBtn = document.querySelector('.quick-catering-request__submit');
+
+function inputFocusHandler() {
+  this.style.boxShadow = '0 0 10px 5px rgba(0, 0, 0, 0.3)';
+  this.style.outline = 'none';
+}
+
+function inputBlurHandler() {
+  this.style.boxShadow = 'none';
+}
+
+function submitMouseOverHandler() {
+  this.style.position = 'absolute';
+  const maxX = window.innerWidth - this.offsetWidth;
+  const maxY = window.innerHeight - this.offsetHeight;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  this.style.left = `${randomX}px`;
+  this.style.top = `${randomY}px`;
+}
+
+for (const inputElem of inputElements) {
+  inputElem.addEventListener('focus', inputFocusHandler);
+  inputElem.addEventListener('blur', inputBlurHandler);
+}
+submitBtn.addEventListener('mouseover', submitMouseOverHandler);
